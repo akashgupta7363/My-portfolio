b4e335d1d8dd3e09535a8f3de84b7f2fa392dc72
Akash Gupta <akashgupta7363@gmail.com>
2023-05-06 18:47:22 +0530

    nav part

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./contact.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  return <section id="contact">Contact</section>;
 }
 
 export default Contact;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./nav.css";
+import { AiOutlineHome, AiOutlineProject, AiOutlineUser } from "react-icons/ai";
+import { BiBook, BiMessageDetail } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experiences">
+        <BiBook />
+      </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
+      <a href="#portfolio">
+        <AiOutlineProject />
+      </a>
+      <a href="#contact">
+        <BiMessageDetail />
+      </a>
+    </nav>
+  );
 }
 
 export default Nav;
