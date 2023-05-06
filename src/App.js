ebd73af9c70fed60235d3b60f0e63aaceb5c32ed
Akash Gupta <akashgupta7363@gmail.com>
2023-05-06 14:57:06 +0530

    default css added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experiences/Experience";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Portfolio from "./components/Portfolio/Portfolio";
 
-function App() {
-  return <>APPPPP</>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
