66a95bd37a4352bb8650d5f86dd37888827a5b33
Akash Gupta <akashgupta7363@gmail.com>
2023-05-06 15:20:12 +0530

    header btns

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <header>
+        <div className="container header_container">
+          <h5>Hello I'm</h5>
+          <h1>Akash Kumar Gupta</h1>
+          <h5 className="text-light">Fullstack Developer</h5>
+          <CTA />
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
