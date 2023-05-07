59b988c2aaf36051aa6db74895f5f6b4bc84d399
Akash Gupta <akashgupta7363@gmail.com>
2023-05-07 19:57:14 +0530

    testimonial part done

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,58 +5,75 @@ import av2 from "../../assets/avatar2.jpg";
 import av3 from "../../assets/avatar3.jpg";
 import av4 from "../../assets/avatar4.jpg";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+
 function Testimonials() {
+  const data = [
+    {
+      avatar: av1,
+      name: "Tina Snow",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam delectus vel voluptas autem adipisci explicabo nulla voluptatem rerum officia beatae cumque corporis ea expedita, laudantium dolor aliquid fugiat id consectetur, amet quibusdam nihil?",
+    },
+    {
+      avatar: av2,
+      name: "Luke Skywalker",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam delectus vel voluptas autem adipisci explicabo nulla voluptatem rerum officia beatae cumque corporis ea expedita, laudantium dolor aliquid fugiat id consectetur, amet quibusdam nihil?",
+    },
+    {
+      avatar: av3,
+      name: "Alex lawson",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam delectus vel voluptas autem adipisci explicabo nulla voluptatem rerum officia beatae cumque corporis ea expedita, laudantium dolor aliquid fugiat id consectetur, amet quibusdam nihil?",
+    },
+    {
+      avatar: av4,
+      name: "Robert Frost",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam delectus vel voluptas autem adipisci explicabo nulla voluptatem rerum officia beatae cumque corporis ea expedita, laudantium dolor aliquid fugiat id consectetur, amet quibusdam nihil?",
+    },
+  ];
   return (
     <section id="tesimonials">
       <h5> Review from clients</h5>
       <h2>Testimonials</h2>
-
-      <div className="container testimonials_container">
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={av1} alt="avatar" />
-          </div>
-          <h5>Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ab
-            aspernatur cupiditate, sapiente culpa quos aut veritatis
-            voluptatibus vitae dolore ipsam a inventore?
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={av2} alt="avatar" />
-          </div>
-          <h5>Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ab
-            aspernatur cupiditate, sapiente culpa quos aut veritatis
-            voluptatibus vitae dolore ipsam a inventore?
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={av3} alt="avatar" />
-          </div>{" "}
-          <h5>Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ab
-            aspernatur cupiditate, sapiente culpa quos aut veritatis
-            voluptatibus vitae dolore ipsam a inventore?
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={av4} alt="avatar" />
-          </div>
-          <h5>Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ab
-            aspernatur cupiditate, sapiente culpa quos aut veritatis
-            voluptatibus vitae dolore ipsam a inventore?
-          </small>
-        </article>
-      </div>
+      <Swiper
+        className="container testimonials_container"
+        loop={true}
+        grabCursor={true}
+        spaceBetween={24}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          580: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 1,
+            spaceBetween: 48,
+          },
+        }}
+        modules={[Pagination]}
+      >
+        {data.map((avt, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client_avatar">
+                <img src={avt.avatar} alt="avatar" />
+              </div>
+              <h5>{avt.name}</h5>
+              <small className="client_review">{avt.review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 }
