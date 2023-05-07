8a4d8999ba194467465d4ccba3fcc8fbec8d6497
Akash Gupta <akashgupta7363@gmail.com>
2023-05-07 18:11:28 +0530

    testimonials content

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,64 @@
 import React from "react";
 import "./testimonials.css";
+import av1 from "../../assets/avatar1.jpg";
+import av2 from "../../assets/avatar2.jpg";
+import av3 from "../../assets/avatar3.jpg";
+import av4 from "../../assets/avatar4.jpg";
 
 function Testimonials() {
-  return <section id="tesimonials">Testimonials</section>;
+  return (
+    <section id="tesimonials">
+      <h5> Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials_container">
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={av1} alt="avatar" />
+            <h5>Ernest Achiever</h5>
+            <small className="client_review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ab
+              aspernatur cupiditate, sapiente culpa quos aut veritatis
+              voluptatibus vitae dolore ipsam a inventore?
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={av2} alt="avatar" />
+            <h5>Ernest Achiever</h5>
+            <small className="client_review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ab
+              aspernatur cupiditate, sapiente culpa quos aut veritatis
+              voluptatibus vitae dolore ipsam a inventore?
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={av3} alt="avatar" />
+            <h5>Ernest Achiever</h5>
+            <small className="client_review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ab
+              aspernatur cupiditate, sapiente culpa quos aut veritatis
+              voluptatibus vitae dolore ipsam a inventore?
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={av4} alt="avatar" />
+            <h5>Ernest Achiever</h5>
+            <small className="client_review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ab
+              aspernatur cupiditate, sapiente culpa quos aut veritatis
+              voluptatibus vitae dolore ipsam a inventore?
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Testimonials;
