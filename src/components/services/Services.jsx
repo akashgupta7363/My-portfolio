2dfc83d571cc2676f33af0c359afa76698572e8a
Akash Gupta <akashgupta7363@gmail.com>
2023-05-08 19:42:38 +0530

    skills part added

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./services.css";
+import { icons } from "./skillslist";
 
 function Services() {
   return (
-    <section id="services" className="services_section">
-      <h1 className="section_title"> Services</h1>
+    <section id="services" className="services_section container">
+      <div className="skills">
+        {icons.map((icon, index) => {
+          return (
+            <div className="icon" key={index}>
+              {icon}
+            </div>
+          );
+        })}
+      </div>
+
+      {/* <h1 className="section_title"> Services</h1>
       <span className="section_subtitle">What I offer</span>
       <div className="services_container container grid">
         <div className="services_content">
@@ -12,7 +23,7 @@ function Services() {
             <i className="" />
           </div>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
---
+++ src/components/services/skillslist.js
@@ -0,0 +1,31 @@
+import { FaJava, FaReact, FaNodeJs, FaSass } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { DiCss3, DiMongodb, DiGitMerge, DiNodejs } from "react-icons/di";
+import {
+  SiExpress,
+  SiFramer,
+  SiJavascript,
+  SiMysql,
+  SiSocketdotio,
+  SiTypescript,
+} from "react-icons/si";
+import { TbBrandRedux } from "react-icons/tb";
+
+export const icons = [
+  <FaJava size={70} />,
+  <FaHtml5 size={70} />,
+  <DiCss3 size={70} />,
+  <FaSass size={70} />,
+  <SiJavascript size={70} />,
+  <SiTypescript size={70} />,
+  <FaReact size={70} />,
+  <TbBrandRedux size={70} />,
+  <DiNodejs size={90} />,
+  <SiExpress size={70} />,
+  <DiMongodb size={70} />,
+  <SiMysql size={70} />,
+
+  <SiSocketdotio size={70} />,
+  <DiGitMerge size={70} />,
+  <SiFramer size={70} />,
+];
