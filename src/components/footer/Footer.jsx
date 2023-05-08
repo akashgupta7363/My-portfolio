24039e0064a7b217622841ad947ec8a4e2951728
Akash Gupta <akashgupta7363@gmail.com>
2023-05-08 14:40:33 +0530

    Footer part completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import "./footer.css";
-
+import { IoLogoTwitter } from "react-icons/io";
+import { FiInstagram } from "react-icons/fi";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        {" "}
+        Akash
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#experiences">Qualification</a>
+        </li>
+        <li>
+          <a href="#Services">Skills & Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a
+          href="https://www.linkedin.com/in/akash-kumar-gupta-576337139/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/akashgupta7363?tab=overview&from=2023-05-01&to=2023-05-0 6"
+          target="_blank"
+        >
+          <BsGithub />
+        </a>
+        <a href="https://twitter.com/Akash_Gupta_7" target="_blank">
+          <IoLogoTwitter />
+        </a>
+        <a href="https://www.instagram.com/ig_akash_gupta_/" target="_blank">
+          <FiInstagram />
+        </a>
+      </div>
+      <div className="footer_copyright">
+        <small>&copy; AKASH Portfolio. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
